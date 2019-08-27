e267bc2c2074ee60de72d8e687524d42c184824d
VIctor <victor.kr34@gmail.com>
2019-08-28 01:32:23 +0300

    add build

---
+++ build/precache-manifest.d166d12ae21baa37eedb576dc3ece8c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fd3fd60d3feedf8a4b6207cfedef3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c8deb7e5b4a1963d05d",
+    "url": "/static/css/main.49ee8221.chunk.css"
+  },
+  {
+    "revision": "f6b653731a93cd02e941",
+    "url": "/static/js/2.39bf4e4b.chunk.js"
+  },
+  {
+    "revision": "3c8deb7e5b4a1963d05d",
+    "url": "/static/js/main.0ca0727e.chunk.js"
+  },
+  {
+    "revision": "11dc8605dc7172615265",
+    "url": "/static/js/runtime~main.ea9f783b.js"
+  },
+  {
+    "revision": "76758a29286fae757d4ccaa7d7d575e4",
+    "url": "/static/media/not-found.76758a29.svg"
+  },
+  {
+    "revision": "51638bd0491916ba1f8d4b35933d3ca6",
+    "url": "/static/media/upload-file.51638bd0.svg"
+  }
+]);
